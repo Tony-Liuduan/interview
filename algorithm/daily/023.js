cb14d0234570c6ad9fc2a645650f34650982e1a0
liuduan <liuduan@58.com>
2020-05-28 14:32:14 +0800

    update

--- algorithm/daily/023.js
+++ algorithm/daily/023.js
@@ -1,10 +1,9 @@
 /**
- * @fileoverview 
+ * @fileoverview 023 合并两个排序的链表
  * @author liuduan
  * @Date 2020-05-28 14:30:46
- * @LastEditTime 2020-05-28 14:31:07
+ * @LastEditTime 2020-05-28 14:32:04
  * https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
- * 合并两个排序的链表
  * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
  */
 
