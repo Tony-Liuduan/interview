247b4a4cfbc3592451e85e71d7795fc24439c659
liuduan <liuduan@58.com>
2020-05-29 18:40:19 +0800

    add flex 圣杯

--- algorithm/daily/024.js
+++ algorithm/daily/024.js
@@ -2,7 +2,7 @@
  * @fileoverview 024 和为s的连续正数序列
  * @author liuduan
  * @Date 2020-05-29 17:18:34
- * @LastEditTime 2020-05-29 17:19:05
+ * @LastEditTime 2020-05-29 17:56:05
  * https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
  * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
  * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
@@ -34,5 +34,37 @@ var findContinuousSequence = function (target) {
         }
     }
 
+    return res;
+};
+
+
+/**
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function (target) {
+    if (target < 3) {
+        return [];
+    }
+    // 双指针法，从1，2开始计算
+    let a = 1;
+    let b = 2;
+    let sum = a + b;
+    let res = [];
+    let middle = Math.ceil(target / 2);
+
+    while (a !== b && b <= middle) {
+        if (sum === target) {
+            res.push(Array.from({ length: b - a + 1 }, (item, i) => i + a));
+            b++;
+            sum += b;
+        } else if (sum > target) {
+            sum -= a;
+            a++;
+        } else {
+            b++;
+            sum += b;
+        }
+    }
     return res;
 };
